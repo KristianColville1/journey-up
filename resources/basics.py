e1343e4faefff64719da82389f9f9cc213085726
Kristian Philip Colville <kristiancolville1@gmail.com>
2021-12-12 19:44:37 +0000

    add info to file

--- resources/basics.py
+++ resources/basics.py
@@ -1,4 +1,24 @@
 # python basics
 
 #print
-print('This will print out in the terminal')
+print('This will print out in the terminal')    # this is a function, print(), text within is a parameter
+
+print("We can't forget.. Hello, World") # single or double quotes, preference...
+
+# some variable types
+string1 = 'This is a string'
+string2 = 'This is another string'
+
+int1 = 1 # this is an integer - whole number
+float1 = 0.5 # this is a floating point - decimal number
+
+# string manipulation and code intelligence
+print('this is a line\nthis is a line\nthis is a line') # the \n creates a new line
+
+print('this is a line' + ' and this is another line') # string concatenation, using the + operator
+
+# more functions
+#input('What is your name?') # takes user input
+
+user = input("What is your name?") # assigning the users value to our user object
+print("I hope you enjoy coding " + user) # prints to the terminal and prints user value, their name
